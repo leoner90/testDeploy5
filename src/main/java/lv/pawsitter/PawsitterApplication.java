24f7b80673dbd150fc9af5ecc39fe14c5fa8d4d4
Leonids Gurockins <leonid.gurockin@gmail.com>
2026-07-10 23:25:47 +0300

    init clean project

---
+++ src/main/java/lv/pawsitter/PawsitterApplication.java
@@ -0,0 +1,13 @@
+package lv.pawsitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawsitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawsitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lv/pawsitter/PawsitterApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.pawsitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawsitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
